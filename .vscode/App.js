e92dc8ad6d47a879c565c536636baa757c85b731
kimaryean <ehdghlove0330@naver.com>
2019-08-01 17:32:28 +0900

    react1-2

---
+++ .vscode/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+    reder(){
+        return (
+            <div className="App">
+                Hello React
+            </div>
+        );
+    }
+}
+export default App;
